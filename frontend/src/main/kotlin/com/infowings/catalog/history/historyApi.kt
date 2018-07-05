05e380e1691cfadcc878a6503d52e47d14069fac
Sergey Reznick <sr@air-consulting.ru>
2018-07-05 18:02:09 +0300

    KS-200: some timestamp output on frontend side

--- frontend/src/main/kotlin/com/infowings/catalog/history/historyApi.kt
+++ frontend/src/main/kotlin/com/infowings/catalog/history/historyApi.kt
@@ -3,18 +3,32 @@ package com.infowings.catalog.history
 import com.infowings.catalog.common.*
 import com.infowings.catalog.utils.get
 import kotlinx.serialization.json.JSON
+import kotlin.js.Date
 
 suspend fun getAllEvents(): List<HistoryData<*>> {
+    val t1 = Date.now()
     val aspectEvents = getAllAspectEvents()
+    val t2 = Date.now()
     val refBookEvents = getAllRefBookEvents()
+    val t3 = Date.now()
     val objectEvents = getAllObjectEvents()
+    val t4 = Date.now()
     val subjectEvents = getAllSubjectEvents()
+    val t5 = Date.now()
 
 
     /* Здесь нам надо смреджить два сортированных списка. Полагаемся на то, что используется реализация merge sort,
      * умеющая распознавать отсортированные подсписки. Кажется в JDK так и есть, а котлиновские коллекции - обертки
       * над JDK. Если это вдруг не так, поменяем */
-    return (aspectEvents + refBookEvents + objectEvents + subjectEvents).sortedByDescending { it.event.timestamp }
+    val res = (aspectEvents + refBookEvents + objectEvents + subjectEvents).sortedByDescending { it.event.timestamp }
+    val t6 = Date.now()
+
+    val times = listOf(t1, t2, t3, t4, t5, t6)
+    println("times: " + times)
+
+    println("time ranges: " + times.zipWithNext())
+
+    return res
 }
 
 suspend fun getAllAspectEvents(): List<AspectHistory> =
